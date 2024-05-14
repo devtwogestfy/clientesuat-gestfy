e68b0b4a89b0b35dc39ffd59f6cef427a2b522e7
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-05-14 13:56:28 -0400

    charts

--- src/views/dashboard/BajajAreaChartCard.jsx
+++ src/views/dashboard/BajajAreaChartCard.jsx
@@ -5,98 +5,105 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import ApexCharts from 'apexcharts';
 import Chart from 'react-apexcharts';
-import chartData from './chart-data/bajaj-area-chart';
 import { useTheme } from '@mui/material/styles';
 import GetInfoService from 'configuraciones/servicios/info-client';
 
 const BajajAreaChartCard = () => {
     const theme = useTheme();
     const orangeDark = theme.palette.secondary[800];
-    const [chartOptions, setChartOptions] = useState(chartData.options);
     const [data, setData] = useState([]);
     const customization = useSelector((state) => state.customization);
     const { navType } = customization;
 
     useEffect(() => {
         const newSupportChart = {
-            ...chartData.options,
             colors: [orangeDark],
             tooltip: { theme: 'light' }
         };
         ApexCharts.exec(`support-chart`, 'updateOptions', newSupportChart);
-        setChartOptions(newSupportChart);
         fetchData();
     }, [navType, orangeDark]);
 
-    // Función para actualizar los datos del gráfico
-
     const fetchData = async () => {
         try {
-            GetInfoService()
-                .getBilling()
-                .then((billing) => {
-                    setData(billing);
-                });
+            const billing = await GetInfoService().getBilling();
+            setData(billing);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
-    console.log(data);
-     const months = data.map(item => item.mes);
-    const series = [{
-      name: 'Suma',
-      data: data.map(item => parseFloat(item.suma)),
-    }];
+
+    const months = data.map((item) => item.mes);
+    const series = [
+        {
+            name: 'Suma',
+            data: data.map((item) => parseFloat(item.suma))
+        }
+    ];
 
     const options = {
-        type: 'area',
-        height: 95,
-        options: {
-            chart: {
-                id: 'support-chart',
-                sparkline: {
-                    enabled: true
-                }
-            },
-            labels: months,
-            dataLabels: {
+        chart: {
+            id: 'support-chart',
+            sparkline: {
+                enabled: true
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth',
+            width: 1
+        },
+        xaxis: {
+            categories: months
+        },
+        yaxis: {
+            opposite: true
+        },
+        legend: {
+            horizontalAlign: 'left'
+        },
+        tooltip: {
+            fixed: {
                 enabled: false
             },
-            stroke: {
-                curve: 'smooth',
-                width: 1
+            x: {
+                show: true
             },
-            xaxis: {
-              type: 'datetime',
+            y: {
+                title: {}
             },
-            yaxis: {
-              opposite: true
-            },
-            legend: {
-              horizontalAlign: 'left'
-            },
-            tooltip: {
-                fixed: {
-                    enabled: false
-                },
-                x: {
-                    show: true
-                },
-                y: {
-                    title: {
-                        months
-                    }
-                },
-                marker: {
-                    show: false
-                }
+            marker: {
+                show: false
             }
         }
     };
 
     return (
         <Card sx={{ bgcolor: 'secondary.light' }}>
-            <Chart options={options} series={series} type="area" height={180} />
+            <Grid container sx={{ p: 2, pb: 0, color: '#fff' }}>
+                <Grid item xs={12}>
+                    <Grid container alignItems="center" justifyContent="space-between">
+                        <Grid item>
+                            <Typography variant="subtitle1" sx={{ color: 'secondary.dark' }}>
+                                Bajaj Finery
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h4" sx={{ color: 'grey.800' }}>
+                                $1839.00
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="subtitle2" sx={{ color: 'grey.800' }}>
+                        10% Profit
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Chart options={options} series={series} type="area" height={95} />
         </Card>
     );
 };
