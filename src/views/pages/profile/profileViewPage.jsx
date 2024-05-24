690d18f6e03346c16b01af3de1487e006db1efda
kevindurangestfy <140419568+kevindurangestfy@users.noreply.github.com>
2024-05-24 08:09:48 -0500

    prototipo de componente perfil

--- src/views/pages/profile/profileViewPage.jsx
+++ src/views/pages/profile/profileViewPage.jsx
@@ -1,61 +1,160 @@
 import React from 'react';
-import { Avatar, Box, Button, Card, CardContent, Container, Grid, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { Link } from 'react-router-dom';
+import {
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
+    Typography,
+    Avatar,
+    Box,
+    Stack,
+    Button,
+    Grid,
+    Tabs,
+    Tab,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText
+} from '@mui/material';
+import ProfileBackground from 'assets/images/banner.png';
+import ProfileImage from 'assets/images/users/user-round.svg';
+import MainCard from 'ui-component/cards/MainCard';
+import HomeIcono from '@mui/icons-material/Home';
+import SendIcon from '@mui/icons-material/Send';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import PinDropIcon from '@mui/icons-material/PinDrop';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 
-const useStyles = makeStyles((theme) => ({
-    avatar: {
-        width: theme.spacing(10),
-        height: theme.spacing(10)
-    },
-    button: {
-        marginRight: theme.spacing(2)
-    }
-}));
+// eslint-disable-next-line react/prop-types
+function CustomTabPanel({ children, value, index }) {
+    return (
+        <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`}>
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
 
 const ProfileViewPage = () => {
-    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
-        <Container maxWidth="lg">
-            <Box mt={4}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Typography variant="h3" gutterBottom>
-                            Mi Perfil
-                        </Typography>
-                        <Card>
-                            <CardContent>
-                                <Grid container spacing={3} alignItems="center">
-                                    <Grid item xs={12} md={3} align="center">
-                                        <Avatar className={classes.avatar} src="/path/to/avatar.jpg" alt="Avatar" />
-                                    </Grid>
-                                    <Grid item xs={12} md={9}>
-                                        <Typography variant="h5" gutterBottom>
-                                            Información Personal
-                                        </Typography>
-                                        <Typography variant="body1">Nombre: Juan Pérez</Typography>
-                                        <Typography variant="body1">Correo Electrónico: juan@example.com</Typography>
-                                        <Typography variant="body1">Teléfono: 1234567890</Typography>
-                                        <Box mt={2}>
-                                            <Button
-                                                variant="contained"
-                                                color="primary"
-                                                component={Link}
-                                                to="/edit-profile"
-                                                className={classes.button}
-                                            >
-                                                Contacto
-                                            </Button>
-                                        </Box>
-                                    </Grid>
-                                </Grid>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
+        <MainCard>
+            <Box position="relative">
+                <Card>
+                    <CardActionArea>
+                        <CardMedia component="img" height="240" image={ProfileBackground} sx={{ borderRadius: '10px' }} />
+                        <Box position="absolute" top="120px" left="16px" zIndex={1}>
+                            <Avatar
+                                src={ProfileImage}
+                                sx={{
+                                    width: 150,
+                                    height: 150,
+                                    border: '2px solid white',
+                                    boxShadow: 2
+                                }}
+                            />
+                        </Box>
+                    </CardActionArea>
+                    <CardContent sx={{ pt: '60px' }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={8} md={6}>
+                                <Typography gutterBottom variant="h2" component="div">
+                                    Kevin Duran
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={4} md={6} container justifyContent="flex-end">
+                                <Stack direction="row" spacing={2}>
+                                    <Button variant="outlined" startIcon={<HomeIcono />}>
+                                        Inicio
+                                    </Button>
+                                    <Button variant="contained" endIcon={<SendIcon />}>
+                                        Nueva Incidencia
+                                    </Button>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                </Card>
             </Box>
-        </Container>
+            <Box sx={{ width: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Datos" id="simple-tab-0" aria-controls="simple-tabpanel-0" />
+                        <Tab label="Servicios" id="simple-tab-1" aria-controls="simple-tabpanel-1" />
+                        <Tab label="Facturas" id="simple-tab-2" aria-controls="simple-tabpanel-2" />
+                        <Tab label="Incidencias" id="simple-tab-3" aria-controls="simple-tabpanel-3" />
+                    </Tabs>
+                </Box>
+                <CustomTabPanel value={value} index={0}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={4} md={4}>
+                            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                                <nav aria-label="main mailbox folders">
+                                    <List>
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <InboxIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Inbox" />
+                                            </ListItemButton>
+                                        </ListItem>
+
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <PhoneAndroidIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Telefono" /> (+51) 934356241
+                                            </ListItemButton>
+                                        </ListItem>
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <DraftsIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Email" /> correo@gmail.com
+                                            </ListItemButton>
+                                        </ListItem>
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <PinDropIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Dirección" /> Lima - Peru
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </List>
+                                </nav>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={8} md={8}>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17020.630463585374!2d-77.05637115593359!3d-12.04374503007184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c5f619ee3ec7%3A0x14206cb9cc452e4a!2sLima!5e0!3m2!1ses!2spe!4v1716499049289!5m2!1ses!2spe" width="100%" height="200"></iframe>
+                        </Grid>
+                    </Grid>
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={1}>
+                    Servicios
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={2}>
+                    Facturas
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={3}>
+                    Incidencias
+                </CustomTabPanel>
+            </Box>
+        </MainCard>
     );
 };
 
