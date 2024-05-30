6e8ccaf6393cde4bd92d5a260f60fe01fae323d7
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-05-30 10:51:17 -0400

    ajuste incidents

--- src/views/pages/incidents/StatusColor.jsx
+++ src/views/pages/incidents/StatusColor.jsx
@@ -4,18 +4,18 @@ import PropTypes from 'prop-types';
 import { Badge } from '@mui/material';
 
 function StatusColor({ estado_id }) {
-    const color = estado_id == 1 ? 'error' : estado_id == 2 ? 'warning' : estado_id == 3 ? 'success' : '';
-    const name = estado_id == 1 ? 'Abierta' : estado_id == 2 ? 'En Espera' : estado_id == 3 ? 'Cerrada' : '';
+  const color = estado_id === '1' ? 'error' : estado_id === '2' ? 'warning' : estado_id === '3' ? 'success' : '';
+  const name = estado_id === '1' ? 'Abierta' : estado_id === '2' ? 'En Espera' : estado_id === '3' ? 'Cerrada' : '';
 
-    return (
-        <Box sx={{ width: '100%', textAlign: 'center' }}>
-            <Badge badgeContent={name} color={color}></Badge>
-        </Box>
-    );
+  return (
+    <Box sx={{ width: '100%', textAlign: 'center' }}>
+      <Badge badgeContent={name} color={color}></Badge>
+    </Box>
+  );
 }
 
 StatusColor.propTypes = {
-    estado_id: PropTypes.number
+  estado_id: PropTypes.string
 };
 
 export default StatusColor;
