a0e8ba1e2226478a703d603950545720c7a23f4e
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-05-30 12:42:37 -0400

    color

--- src/utils/dataGridStyles.js
+++ src/utils/dataGridStyles.js
@@ -1,14 +1,22 @@
+import GetCustomization from 'services/customizeService';
+
+const customization = await GetCustomization();
+let textColor = '#fff';
+if (customization.color === '#FFFF00') {
+  textColor = '#000000';
+}
 const dataGridStyles = (theme) => ({
-    root: {
-        height: 400,
-        width: '100%'
-    },
-    dataGrid: {
-        '& .MuiDataGrid-columnHeaders': {
-            backgroundColor: theme.palette.secondary.dark,
-            color: '#fff'
-        }
+  root: {
+    height: 400,
+    width: '100%'
+  },
+  dataGrid: {
+    '& .MuiDataGrid-columnHeaders': {
+      //backgroundColor: theme.palette.secondary.dark,
+      backgroundColor: customization.color,
+      color: textColor
     }
+  }
 });
 
 export default dataGridStyles;
