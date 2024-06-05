295da15096e3786ccac96e9a86741d684658e8cc
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-06-04 20:04:06 -0400

    validate

--- src/views/pages/profile/profileViewPage.jsx
+++ src/views/pages/profile/profileViewPage.jsx
@@ -16,6 +16,7 @@ import { createIncident } from 'settings/servicios/incident';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import GetCustomization from 'services/customizeService';
+import CircularWithValueLabel from 'views/utilities/CircularProgressWithLabel';
 const customization = await GetCustomization();
 
 function CustomTabPanel({ children, value, index }) {
@@ -34,12 +35,14 @@ const ProfileViewPage = () => {
   const infoClient = GetInfoClient();
   const [error, setError] = useState('');
   const [openSuccess, setOpenSuccess] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   const fetchData = async () => {
     try {
       infoClient.getClient().then((client) => {
         setClientInfo(client);
       });
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -95,76 +98,84 @@ const ProfileViewPage = () => {
 
   return (
     <MainCard>
-      <Box position="relative">
-        <Card>
-          <CardActionArea>
-            <CardMedia component="img" height="240" image={ProfileBackground} sx={{ borderRadius: '10px' }} />
-            <Box position="absolute" top="120px" left="16px" zIndex={1}>
-              <Avatar
-                src={ProfileImage}
-                sx={{
-                  width: 150,
-                  height: 150,
-                  border: '2px solid white',
-                  boxShadow: 2
-                }}
-              />
+      {isLoading ? (
+        <div className="center-spinner">
+          <CircularWithValueLabel color="secondary" />
+        </div>
+      ) : (
+        <>
+          <Box position="relative">
+            <Card>
+              <CardActionArea>
+                <CardMedia component="img" height="240" image={ProfileBackground} sx={{ borderRadius: '10px' }} />
+                <Box position="absolute" top="120px" left="16px" zIndex={1}>
+                  <Avatar
+                    src={ProfileImage}
+                    sx={{
+                      width: 150,
+                      height: 150,
+                      border: '2px solid white',
+                      boxShadow: 2
+                    }}
+                  />
+                </Box>
+              </CardActionArea>
+              <CardContent sx={{ pt: '60px' }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={8} md={6}>
+                    <Typography gutterBottom variant="h2" component="div">
+                      {clientInfo ? `${clientInfo.nombre} ${clientInfo.apellido}` : '-'}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={4} md={6} container justifyContent="flex-end">
+                    <Stack direction="row" spacing={2}>
+                      <Button component={Link} to="/" variant="outlined" startIcon={<HomeIcono />}>
+                        <FormattedMessage id="layout.menu_home" />
+                      </Button>
+                      <Button variant="contained" endIcon={<SendIcon />} onClick={handleOpen}>
+                        <FormattedMessage id="info.contact" />
+                      </Button>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="Datos" id="simple-tab-0" aria-controls="simple-tabpanel-0" />
+                <Tab label="Servicios" id="simple-tab-1" aria-controls="simple-tabpanel-1" />
+                <Tab label="Facturas" id="simple-tab-2" aria-controls="simple-tabpanel-2" />
+                {customization.view_tickets === 1 && <Tab label="Incidencias" id="simple-tab-3" aria-controls="simple-tabpanel-3" />}
+              </Tabs>
             </Box>
-          </CardActionArea>
-          <CardContent sx={{ pt: '60px' }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={8} md={6}>
-                <Typography gutterBottom variant="h2" component="div">
-                  {clientInfo ? `${clientInfo.nombre} ${clientInfo.apellido}` : '-'}
-                </Typography>
-              </Grid>
-              <Grid item xs={12} sm={4} md={6} container justifyContent="flex-end">
-                <Stack direction="row" spacing={2}>
-                  <Button component={Link} to="/" variant="outlined" startIcon={<HomeIcono />}>
-                    <FormattedMessage id="layout.menu_home" />
-                  </Button>
-                  <Button variant="contained" endIcon={<SendIcon />} onClick={handleOpen}>
-                    <FormattedMessage id="info.contact" />
-                  </Button>
-                </Stack>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Box>
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="Datos" id="simple-tab-0" aria-controls="simple-tabpanel-0" />
-            <Tab label="Servicios" id="simple-tab-1" aria-controls="simple-tabpanel-1" />
-            <Tab label="Facturas" id="simple-tab-2" aria-controls="simple-tabpanel-2" />
-            {customization.view_tickets === 1 && <Tab label="Incidencias" id="simple-tab-3" aria-controls="simple-tabpanel-3" />}
-          </Tabs>
-        </Box>
-        <CustomTabPanel value={value} index={0}>
-          <ClientTabPanel clientInfo={clientInfo} />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-          <ServiceTabPanel />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={2}>
-          <InvoiceTabPanel />
-        </CustomTabPanel>
-        {customization.view_tickets === 1 && (
-          <CustomTabPanel value={value} index={3}>
-            <IncidentTabPanel />
-          </CustomTabPanel>
-        )}
-        <MessageDialog
-          open={open}
-          handleClose={handleClose}
-          handleSendMessage={handleSendMessage}
-          message={message}
-          handleChangeMessage={handleChangeMessage}
-          error={error}
-        />
-        <SuccessDialog openSuccess={openSuccess} handleCloseSuccess={handleCloseSuccess} />
-      </Box>
+            <CustomTabPanel value={value} index={0}>
+              <ClientTabPanel clientInfo={clientInfo} />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              <ServiceTabPanel />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+              <InvoiceTabPanel />
+            </CustomTabPanel>
+            {customization.view_tickets === 1 && (
+              <CustomTabPanel value={value} index={3}>
+                <IncidentTabPanel />
+              </CustomTabPanel>
+            )}
+            <MessageDialog
+              open={open}
+              handleClose={handleClose}
+              handleSendMessage={handleSendMessage}
+              message={message}
+              handleChangeMessage={handleChangeMessage}
+              error={error}
+            />
+            <SuccessDialog openSuccess={openSuccess} handleCloseSuccess={handleCloseSuccess} />
+          </Box>
+        </>
+      )}
     </MainCard>
   );
 };
