5a15df47421ba20e23fa25d27cf3eceb52c9fc9f
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-06-06 09:26:06 -0400

    Update format-date.js

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,5 +1,17 @@
 import { format } from 'date-fns';
 
 export function fDate(value) {
-    return format(new Date(value), 'dd/MM/yyyy');
+  if (value == null) {
+    return '';
+  }
+  if (!(value instanceof Date)) {
+    const parsedDate = new Date(value);
+
+    if (!isNaN(parsedDate.getTime())) {
+      return format(parsedDate, 'dd/MM/yyyy');
+    } else {
+      return '';
+    }
+  }
+  return format(value, 'dd/MM/yyyy');
 }
